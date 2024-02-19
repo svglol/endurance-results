f25a0491371e99514dff7ea6a3ef7547b5b0337a
Matthew Trotman <savageguy@gmail.com>
2024-02-19 21:36:35 +1300

    feat: clear storage on data update

--- server/api/scrape/index.ts
+++ server/api/scrape/index.ts
@@ -1,7 +1,18 @@
 export default defineEventHandler(async () => {
-  return await Promise.all([
+  const storage = useStorage('data')
+  const [fiawec, elms, alms] = await Promise.all([
     $fetch('/api/scrape/fiawec'),
     $fetch('/api/scrape/elms'),
     $fetch('/api/scrape/alms'),
   ])
+
+  if (
+    fiawec.updatedFIAWEC > 0 ||
+    elms.updatedELMS > 0 ||
+    alms.updatedALMS > 0
+  ) {
+    storage.clear()
+  }
+
+  return [fiawec, elms, alms]
 })
--- server/api/series/index.get.ts
+++ server/api/series/index.get.ts
@@ -13,6 +13,6 @@ export default defineCachedEventHandler(
     })
   },
   {
-    maxAge: 60 * 60,
+    maxAge: 1440,
   }
 )
