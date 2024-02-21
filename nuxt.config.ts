5b3bf33c8fd8fe94f4c06ea1f9228ab37fce0e6b
Matthew Trotman <savageguy@gmail.com>
2024-02-21 18:18:34 +1300

    fix: isr value

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -105,7 +105,7 @@ export default defineNuxtConfig({
   routeRules: {
     '/': { isr: 60 },
     '/api/*': {
-      isr: false,
+      isr: 60 * 60,
       cache: {
         base: 'vercelKV',
       },
