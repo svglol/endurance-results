72b22f5bb05af268195558c3425e61422df7acb7
Matthew Trotman <savageguy@gmail.com>
2024-02-27 10:40:54 +1300

    fix: update clearing storage

--- server/utils/shared.ts
+++ server/utils/shared.ts
@@ -4,10 +4,5 @@ export const db = database
 
 export async function clearStorage() {
   const storage = useStorage('data')
-  const keys = await storage.getKeys()
-  for (const key of keys) {
-    if (key.startsWith('nitro:')) {
-      await storage.removeItem(key)
-    }
-  }
+  await storage.clear()
 }
